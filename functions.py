5cb7ec8ba8eaa767e17ac9829a41bd22e7941974
kanounilotfipro <29700274-kanounilotfipro@users.noreply.replit.com>
2024-02-22 20:34:57 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+
+def create_assistant(client):
+  assistant_file_path = 'assistant.json'
+
+  if os.path.exists(assistant_file_path):
+    with open(assistant_file_path, 'r') as file:
+      assistant_data = json.load(file)
+      assistant_id = assistant_data['assistant_id']
+      print("Loaded existing assistant ID.")
+  else:
+    file = client.files.create(file=open("knowledge.docx", "rb"),
+                               purpose='assistants')
+
+    assistant = client.beta.assistants.create(instructions="""
+          Assisting junior sales with information about procedures, prices, and duration of stay.
+The assistant, has been programmed to help junior sales reps with learning company standard operating procedures and selling techniques as a salesperson.
+will be providing prices and details about the procedures
+ will be creating some basic treatment plans according to the examples in the document.
+          """,
+                                              model="gpt-4-1106-preview",
+                                              tools=[{
+                                                  "type": "retrieval"
+                                              }],
+                                              file_ids=[file.id])
+
+    with open(assistant_file_path, 'w') as file:
+      json.dump({'assistant_id': assistant.id}, file)
+      print("Created a new assistant and saved the ID.")
+
+    assistant_id = assistant.id
+
+  return assistant_id
